48051c139a1ea565ec08c31a393fa4da9807b6ce
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-05-04 21:27:43 +0600

    logo styling

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+function Home(){
+  return(
+    <article>
+      <h1>This is Home page</h1>
+      <p> Rem eum, officia veritatis ducimus qui voluptates esse molestiae labore fugiat facere. Voluptates, repudiandae dolore.</p>
+    </article>
+  );
+}
+export default Home;
