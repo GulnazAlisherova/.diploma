d6990391c4f80c68e7164d4c7d837e6759f764ce
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-05-18 14:04:07 +0600

    added import

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,22 @@
 import Header from "../components/Header/Header";
-import headerImage from "../assets/headerr.png";
+import headerImage from "../assets/home.jpeg";
 import CategoryList from "../components/CategoryList/CategoryList";
 import { getCategories } from "../data/categories";
-
-function Home(){
-  return(
+function Home() {
+  return (
     <>
-    <Header
-      title="Welcome to our Pottery Studio!"
-      image={headerImage}>
-    Pottery is the process and the products of forming vessels and other objects with clay and other ceramic materials, which are fired at high temperatures to give them a hard and durable form.
-    </Header>
-
-    <CategoryList categories={getCategories()} />
+      <Header
+        title="Enjoy Our Delicious Meal"
+        image={headerImage}
+        p="Book a table"
+      >
+        Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam
+        amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit
+        clita duo justo magna dolore erat amet
+      </Header>
+      <CategoryList categories={getCategories()}/>
     </>
   );
 }
+
 export default Home;
