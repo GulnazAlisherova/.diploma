c3c149cbd5d6ecee31ff84121adeeaae75e9fc00
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-05-23 10:57:42 +0600

    added new informations for contact/footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,9 +2,37 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <footer className="Footer">
-     Ceramic 2022
+    
+    <div>
+    <footer className="Footer">Ceramic 2022
+      {/* <div className="text">
+         <ul>
+         <li><a href="/">WEEBLY THEMES</a></li>
+         <li><a href="/">PRE-SALE FAQS</a></li>
+         <li><a href="/">SUBMIT A TICKET</a></li>
+       </ul>
+       <ul>
+         <li><a href="/">SERVICES</a></li>
+         <li><a href="/">THEME TWEAK</a></li>
+       </ul>
+       <ul>
+       <li><a href="/">SHOWCASE</a></li>
+       <li><a href="/">SUPPORT</a></li>
+       <li><a href="/">WIDGETKIT</a></li>
+       </ul>
+       </div> */}
+       <div className="icons">
+         <ul>
+           <li><a href="/"><i class="fa-brands fa-facebook-f"></i></a></li>
+           <li><a href="/"><i class="fa-brands fa-twitter"></i></a></li>
+           <li><a href="/"><i class="fa-brands fa-google-plus-g"></i></a></li>
+           <li><a href="/"><i class="fa-brands fa-instagram"></i></a></li>
+         </ul>
+         ©Copyright All rights reserved.
+       </div>
     </footer>
+    
+    </div>
   );
 }
 export default Footer; 
--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -5,10 +5,25 @@ function Contacts() {
   return (
     <>
       <Header
-        title="Order your ceramic!"
+        title="Get in touch"
         image={headerImage}>
-        Ceramics encompass such a vast array of materials that a concise definition is almost impossible. However, one workable definition of ceramics is a refractory, inorganic, and nonmetallic material. Ceramics can be divided into two classes: traditional and advanced. 
+       Want to get in touch? We'd love to hear from you. Here's  how you can reach us...
       </Header>
+     <div className="full-card">
+     <div className="card">
+          <img src="http://www.clker.com/cliparts/f/D/Z/i/d/G/phone-logo-hi.png" alt="picture of phone"/>
+          <h3>Talk to sales</h3>
+          <p>Want to order your type of Ceramic? Just pick up the phone to chat with a member of our team.</p>
+          <h4>+996 789 987 00</h4>
+      </div>
+      
+        <div className="card">
+          <img src="https://pngimage.net/wp-content/uploads/2018/06/message-symbol-png-7.png" alt=" picture of message"/>
+          <h3>Send a message for customer</h3>
+          <p>Want to order your type of Ceramic? Just pick up the phone to chat with a member of our team.</p>
+          <h4>+996 789 987 00</h4>
+        </div>
+     </div>
     </>
   );
 }
