b1a935b79ec67c73eff54ea433301b7b2d15ca46
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-06-17 14:01:54 +0600

    added new components

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,11 +3,14 @@ import NavItem from "./NavItem/NavItem";
 
 function Nav() {
   return (
+    
     <ul className={classes.Nav}>
       <NavItem url="/">Home</NavItem>
       <NavItem url="/about">About</NavItem>
       <NavItem url="/products">All Products</NavItem>
       <NavItem url="/contacts">Contacts</NavItem>
+      
+      {/* <i  class="fa-solid fa-cart-shopping " ></i> */}
     </ul>
   );
 }
