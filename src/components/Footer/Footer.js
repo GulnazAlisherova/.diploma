1701caed1cd2856876e8d55e71fe424abed2a43e
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-06-28 21:26:19 +0600

    added electronic card icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,17 +3,30 @@ import "./Footer.css";
 function Footer() {
   return (
     <div>
-      <footer className="Footer">© 2022 Alisherova Gulnaz
+      <footer className="Footer">
+          ©Copyright All rights reserved.
         <div className="icons">
-          <ul>
+         <div className="flex">
+         <ul>
             <h3>Find us on:</h3>
             <li><a href="https://www.facebook.com/"><i class="fa-brands fa-facebook-f"></i></a></li>
             <li><a href="https://www.twitter.com/"><i class="fa-brands fa-twitter"></i></a></li>
             <li><a href="https://www.google.com/"><i class="fa-brands fa-google-plus-g"></i></a></li>
             <li><a href="https://www.instagram.com/"><i class="fa-brands fa-instagram"></i></a></li>
           </ul>
-          ©Copyright All rights reserved.
+          <div className="elcards">
+          <ul>
+            <h3>We accept:</h3>
+            <li><i class="fa-brands fa-cc-apple-pay"></i></li>
+            <li><i class="fa-brands fa-google-pay"></i></li>
+            <li><i class="fa-brands fa-amazon-pay"></i></li>
+            <li><i class="fa-brands fa-cc-paypal"></i></li>
+            <li><i class="fa-brands fa-cc-visa"></i></li>
+          </ul>
+          </div>
         </div>
+        </div>
+          2022 Alisherova Gulnaz KGZ
       </footer>
 
     </div>
