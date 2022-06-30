87d98ee1b8354215e30dca8134b175a20d043908
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-06-30 08:40:33 +0600

    changed places

--- src/components/ProductList/ProductItem/ProductItem.js
+++ src/components/ProductList/ProductItem/ProductItem.js
@@ -3,17 +3,20 @@ import classes from "./ProductItem.module.css";
 function ProductItem({ product }) {
   return (
     <div className={classes.ProductItem}>
-     <div className={classes.card5}>
-     <Link to={"/products/" + product.productId}>
-        <div className={classes.price}>${product.price}</div>
-        <div className={classes.ProductItemImage}>
-          <img src={product.image} alt={product.title} />
-          <div className={classes.ProductItemText}>
-            <h3>{product.title}</h3>
-          </div>
+      <div className={classes.effectToRight}>
+        <div className={classes.card5}>
+          <Link to={"/products/" + product.productId}>
+
+            <div className={classes.ProductItemImage}>
+              <img src={product.image} alt={product.title} />
+              <div className={classes.ProductItemText}>
+                <h3>{product.title}</h3>
+                <div className={classes.price}>${product.price}</div>
+              </div>
+            </div>
+          </Link>
         </div>
-      </Link>
-    </div>
+      </div>
     </div>
   );
 }
